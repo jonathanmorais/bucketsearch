9a2c30395d77b514abdd2ad827a7bd79a33a4c41
jonathanmorais <jonathan.m.lucena@gmail.com>
2021-12-26 19:09:33 -0300

    feat: add unit test for count response object

---
+++ tests/test_main.py
@@ -0,0 +1,27 @@
+import unittest
+
+class TestJsonResponse(unittest.TestCase):
+
+    def count_array_response():
+        c = []
+        obj = {
+            "bucket-name": [
+                "2019-04-29-20:36:54",
+                "30",
+                "4161"
+                ]
+            }
+
+        for i in obj['bucket-name']:
+            c.append(i)
+
+        return len(c)
+
+    def test_count_array_response(self):
+        count_response = TestJsonResponse.count_array_response()
+        self.assertEqual(count_response, 3)    
+
+if __name__ == '__main__':
+    count = TestJsonResponse
+    count.count_array_response()
+    unittest.main()
