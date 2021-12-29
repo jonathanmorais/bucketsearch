0659d09a5e6c1aa77c13b4222ee89f9094948156
jonathanmorais <jonathan.m.lucena@gmail.com>
2021-12-29 15:24:26 -0300

    fix print help msg

--- src/s3ctl/main.py
+++ src/s3ctl/main.py
@@ -1,5 +1,6 @@
 import logging
 import click
+from click.decorators import command
 from cli import bucket_summarize
 from cli import bucket_summarize
 
@@ -7,10 +8,21 @@ logging.info('s3ctl working')
 
 try:
     @click.command()
-    @click.option('--bucket')
-    def output(bucket):
-        click.echo(bucket_summarize([bucket]))
+    @click.option('--bucket', help='flag to recover bucket info.')
+    def help(name):
+        """Simple CLI that get buckets info."""
+        click.echo('Hello %s!' % name)
 
-    output()
+    def print_help_msg(command):
+        with click.Context(command) as ctx:
+            click.echo(command.get_help(ctx))
+    print_help_msg(help)        
+
+    @click.command()
+    @click.option('--bucket')
+    def bucket(bucket):
+        click.echo(bucket_summarize([bucket]))
+    bucket()    
+
 except click.ClickException as err:
     print(err)
